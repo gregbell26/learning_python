29b0101056a377b7cf13d985bc507e3d9cdc4d94
Gregory <gjbell@hackbookpro.local>
2021-08-28 16:30:32 -0600

    Wrote completely broken algorithm

--- searching_algorithm/main.py
+++ searching_algorithm/main.py
@@ -1,16 +1,31 @@
+def binary_search(arr, number):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
 
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if(arr[mid] < number):
+            low = mid + 1
 
+        elif(arr[mid] > number):
+            high = mid - 1
 
+        else:
+            return mid;
 
-
-
-
-
+        return -1
 
 
 with open("inputs.in") as file:
     content = file.read().splitlines()
 
+for x in range(len(content) - 1):
+    content[x] = int(content[x])
 
-numberToFind = input("Enter a number")
+numberToFind = int(input("Enter a number"))
 
+result = binary_search(content, numberToFind)
+
+print("Number found at index, ", result)
